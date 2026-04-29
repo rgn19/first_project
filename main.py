dbc9374b32d631c0f5e1f07ab3fb1210773d3c9a
Ivan Rozum <rozum.iv03@gmail.com>
2026-04-29 17:20:16 +0300

    Test

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello from repository!')
+from dotenv import load_dotenv
+import os
+
+def print_author():
+    load_dotenv(dotenv_path='.env')
+    author = os.getenv("AUTHOR")
+    print(f"Автор проекта: {author}")
+
+print_author()
